5f6d84a324baa92bc2f96eca386d993d943e4db2
Aloha Nordic <alohanordic@gmail.com>
2016-11-20 16:01:27 -0800

    Oops stacking contexts are fun

--- pages/photo_grid.js
+++ pages/photo_grid.js
@@ -5,9 +5,9 @@
 function onPhotoClick(event) {
   document.getElementById('overlay').style.display = 'block';
   var parent = event.currentTarget;
-  var description = parent.getElementsByClassName('description')[0];
-  this.currentDescription = description;
-  description.style.display = 'block';
+  var id = parent.id;
+  var description = descriptions[id];
+  document.getElementById('description').textContent = description;
 }
 
 /**
@@ -16,6 +16,9 @@ function onPhotoClick(event) {
  */
 function onOverlayClick(event) {
   event.preventDefault();
-  this.currentDescription.style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
+}
+
+var descriptions = {
+  'allison': 'Description of Allison'
 }
