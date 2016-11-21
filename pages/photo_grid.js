7b3b49e3b70915509d069b6906389292ecd92d85
Aloha Nordic <alohanordic@gmail.com>
2016-11-20 17:20:19 -0800

    Add results for the rest of the races

--- pages/photo_grid.js
+++ pages/photo_grid.js
@@ -53,5 +53,44 @@ var results = {
   	'Miles – 29:05 – 13th place',
   	'Tom – 31:54 – 25th place',
   	'Sergey – 31:55 – 26th place'
+  ],
+  'skio-jan-16': [
+  	'Leslie – Advanced Long Blue Course – 95:18 – 11th place'
+  ],
+  'tahoe-rim-16': [
+	'Miles – 1:32:01 – 4th place',
+	'Tom – 1:39:19 – 10th place',
+	'Sergey – 1:43:33 – 13th place',
+	'Leslie – 2:09:44 – 49th place; 13th female',
+	'Allison – 2:35:52 – 71st place; 30th female',
+	'Megan – 2:43:16 – 74th place; 31st female',
+	'Caryn – 3:20:33 – 52nd place; 16th female (Classic)',
+	'Fannie – 3:23:49 – 54th place; 17th female (Classic)'
+  ],
+  'presidents-16': [
+  	'Miles – 45:57 – 9th place',
+	'Mike – 47:48 – 14th place'
+  ],
+  'skio-feb-16': [
+  	'Laura – Orange Course – 49:52 – 1st place',
+	'Leslie & Rachel – Green Course – 1:55:58 – 3rd place',
+	'Egan – Blue Course – 1:30:09 – 7th place'
+  ],
+  'great-ski-race-16': [
+  	'Mike Sooder – 1:44:22 – 16th place',
+	'Miles – 1:50:06 – 25th place',
+	'Tom – 1:50:32 – 30th place',
+	'Egan – 1:54:30 – 36th place',
+	'Sergey – 2:06:51 – 69th place',
+	'Laura – 2:31:30 – 152nd place',
+	'Fink – 3:17:19 – 271st place',
+	'Diederik – 3:31:52 – 298th place',
+	'Matt (classics) – 3:40:13 – 307th place',
+	'Kelly + Leo – 3:46:06 – 317th place'
+  ],
+  'golden-gate-16': [
+  	'Tom - 1:28:08',
+  	'Kalina - 1:51:11',
+  	'Megan - 1:58:33'
   ]
 }
