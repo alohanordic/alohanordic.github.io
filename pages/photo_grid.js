5478078c0fb4b090e443ae19178b84e935f5429d
Aloha Nordic <alohanordic@gmail.com>
2017-03-20 22:20:16 -0700

    Add Bend, GSR, Gold Rush

--- pages/photo_grid.js
+++ pages/photo_grid.js
@@ -53,6 +53,7 @@ var bios = {
   'elliott': 'According to Strava, Elliott has skied more kms than any other person in Bend.  Elliott\'s energetic out-of-the-blue email invite has propelled all of the ladies of Aloha Nordic to not only attend the Bend race, but to move permanently to the Pacific Northwest.  "We want to get a bite out of that Heath bar" - Anonymous',
   'fannie': 'Every day with Fannie is a fairytale.  We hope Fannie stay stay stays on Aloha forever.  This is the story of us: Fannie summited Lola\'s Lookout the first time she ever tried on skate skis and hasn\'t looked back since.  Fannie has a smile that could light up this whole town and an Instagram game that lights up our whole feeds.  She\'s our positive, smiling, gluten-free Juliet forever and always.',
   'garrett': 'He hasn\'t lost a cross-country ski race since 2002.  He hasn\'t lost a life race ever.  People in the ski cabin warm up just looking at his beard.  His Minnesota charm and good manners have been recruited by diplomats for world conflicts.  In fact, even Petter Northug wishes he were as interesting as Garrett Heath.',
+  'heidi': 'Bio on the way!',
   'jamie': 'Some people in life are afraid of failure. Jamie cavalierly embraces it. Nothing thwarts the cruelty of a difficult jigsaw puzzle like planning to only do the perimeter and then stop there. Perhaps it’s this contrarian attitude that led Jamie to the band of rebels known as #AlohaNordic. At first she was skeptical — turning a questioning brow to the enthusiastic pleas of Tom, Miles and Carl. However, as the light started to shine on the array of apparel options that could exist with Aloha, so too did the light of Jamie’s eyes begin to glow. She may plan to exclude certain pieces of the race course, but isn’t that a brilliant strategy?',
   'juice': 'Juice has been known to filter Aloha emails from her inbox, but we know how she really feels about us... Once she sheds her "backcountry skis" for the real deal, we know her LAX prowess will lead her to victories on and off course.  Fun fact: she loved JT early, but it was clinched by his smooth jam "suit and tie".',
   'laura': 'With the unique ability to sleep in any imaginable position and in any location, Shaner arrives at an early AM start line well rested and with a stomach full of fire (typically augmented by the combination of soup and hot chocolate she “crushes” for breakfast). A direct descendent of the Swedish heroine (and lesser known Nordic ski aficionado) Blenda, she leads her army of #AlohaNordic teammates off the start line and into the fray. A competitor so determined, considering no sport too extreme, she once had worried friends deploy Tahoe Search and Rescue in a hilarious mix-up between “lost in the backcountry” and “laying down my early season aerobic base”.',
@@ -60,6 +61,7 @@ var bios = {
   'maddie': 'A burgeoning artist and endurance athlete, Madeleine, or “Matterhorn” as she’s known in hardcore mountaineering circles, has been known to laugh about climbs that would bring other mortals to tears. Just ask her competitors in The Great Ski Race. Perhaps it’s due to her intense trail running foundation, or perhaps it’s due to the fact that she just likes to laugh a lot. Whatever the reason, she undoubtedly brings the “ha” in #AlohaNordic. It’s no wonder she invented the team’s name.',
   'megan': 'Megan is a straight-up Bomber. Whether it’s on the wide boards in the back bowls or on the skinny skis flying down the fiber board at Tahoe XC, she can never have enough wind flowing through her hair. Some Nordic purists may snicker a bit at her dual focus, but #AlohaNordic welcomed her with open arms, as they were longing for a Bomber to hone their descending tactics. Sometimes, Megan has to be gently reminded to substitute words such as “pow-pow” “Epic” and “Bode Miller” with “high-fluoro” “aerobic capacity” and “Petter Northug.” But this a small price to pay for the extra dimension she brings to the team.',
   'miles': 'Sports fans who call Mike Ditka the greatest player-coach of all time haven’t met Miles Rosen. Miles’ accomplishments as a nordic skier are as impressive and vast as a polar bear’s girth. And on the coaching front, he has helped many Aloha Nordic teamsters achieve PBs. Miles’ favorite pastimes include vigorous hill repeats, building new capillaries by whatever means possible and waxing poetically about Northern Minnesota and female anatomy. When not V2’ing in the mountains, Miles can be found crushing the Old La Honda roller ski record (check out his Strava account for proof).',
+  'nick': 'Bio coming soon!',
   'peter': 'NYC finance bro turned bleeding heart academic and all around Mahalo dude.  If you ever go to a Chinese language trivia night, you want him on your team.  This guy has real Aloha potential - high school xc ski extraordinaire and college varsity crew - if he decides to commit a season to it.',
   'rachel': 'Rachel has been skiing at Royal Gorge for over 20 years.  We knew she was Aloha material when she broke her ski pole by getting it stuck on the Yuba rope tow.  Rachel has promised to dress up like an M&M and deliver candy to xc skiers every Easter.  Sometimes Rachel sleeps in until 1pm and misses the entire day\'s activities, but trust us, it\'s worth it to bring her along',
   'tom': 'Most famous for his romantic plights, Tom is known as the “emotional thrill seeker” of the group. He skis fast and never falls… except in the amorous sense. Once in a cozy irish pub, over countless White Russians and Gin Creamies, he lectured a group of innocent bystanders deep into the night on the toxic repercussions of internet dating in modern society. They laughed, they cried, and at the end they all joined Aloha Nordic. He’s the Elmer’s glue stick to your scrapbook, the vitamin D to your cold and flu season, the milk snort to your laugh… and just one of the stallions you will meet on this amazing ski team.',
@@ -163,28 +165,65 @@ var results = {
   'tahoe-rim-jan-17': {
     'title': 'Tahoe Rim Tour 26k - 2017',
     'times': [
-	'Sooder - 1:26:35 - 5th place',
+	    'Sooder - 1:26:35 - 5th place',
     	'Birkie Joe - 1:28:27 - 6th place',
-	'Miles - 1:29:19 - 7th place',
-	'Egan - 1:33:20 - 9th place',
-	'Tom - 1:33:40 - 10th place',
-	'Peter - 1:41:30 - 18th place',
-	'Shwe - 1:49:57 - 26th place',
-	'Allison - 2:09:25 - 48th place (18th female)',
-	'Leslie - 2:14:36 - 53rd place (21st female)',
-	'Cody - 2:22:06 - 63rd place',
-	'Maddie - 2:23:56 - 67th place (27th female)',
-	'William Zachary - 2:30:18 - 69th place',
-	'Fannie - 2:32:47 - 70th place (28th female)',
-	'Caryn - 2:35:33 - 72nd place (30th female)',
-	'Tex - 2:36:50 - 73rd place (31st female)'
+		'Miles - 1:29:19 - 7th place',
+		'Egan - 1:33:20 - 9th place',
+		'Tom - 1:33:40 - 10th place',
+		'Peter - 1:41:30 - 18th place',
+		'Shwe - 1:49:57 - 26th place',
+		'Allison - 2:09:25 - 48th place (18th female)',
+		'Leslie - 2:14:36 - 53rd place (21st female)',
+		'Cody - 2:22:06 - 63rd place',
+		'Maddie - 2:23:56 - 67th place (27th female)',
+		'William Zachary - 2:30:18 - 69th place',
+		'Fannie - 2:32:47 - 70th place (28th female)',
+		'Caryn - 2:35:33 - 72nd place (30th female)',
+		'Tex - 2:36:50 - 73rd place (31st female)'
     ]
   },
   'boulder-tour': {
     'title': 'Boulder Mountain Tour 34k - 2017',
     'times': [
     	'Birkie Joe - 1:56:00 - 76th place',
-	'Egan - 1:58:36 - 89th place'
+	    'Egan - 1:58:36 - 89th place'
+    ]
+  },
+  'great-ski-race-17': {
+    'title': 'The Great Ski Race 30k - 2017',
+    'times': [
+        'After spending hours digging our cars out of 2 feet of fresh snowfall, the team was late to the starting line, so accurate times aren\'t available!',
+    	'Alex - Survived',
+    	'Allison - Survived',
+    	'Bronwen - Survived',
+    	'Caryn - Survived',
+    	'Chris - Survived',
+    	'Diederick - Survived',
+    	'Miles - Survived',
+    	'Nick - Survived',
+    	'Tom - Survived',
+    	'Wheeler - Survived',
+    	'William Zachary - Survived'
+    ]
+  },
+  'cascade-crest': {
+    'title': 'Cascade Crest 50k Relay',
+    'times': [
+    	'Allison, Elliott, Fannie, and Leslie - 5th place'
+    ]
+  },
+  'gold-rush-17': {
+    'title': 'Gold Rush 15, 30, and 45k',
+    'times': [
+    	'(15k) Miles - 45:32 - 1st place',
+    	'(30k) Egan - 1:41 - 2nd place',
+    	'(30k) Peter - 1:41:56 - 3rd place',
+    	'(30k) Bronwen - 1:47:14 - 1st place (female)',
+    	'(30k) William Zachary - 2:29:23 - 18th place',
+    	'(45k) Birkie Joe - 2:42:47 - 20th place',
+    	'(45k) Leslie - 4:23:12 - 10th place (female)',
+    	'(45k) Allison - 4:25:53 - 11th place (female)',
+    	'(45k) Nick - Race was interrupted'
     ]
   }
 }
